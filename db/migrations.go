e3714595ec06c74e2760749981972534c5262ddb
MariaVabiolla <mariavabiolla12@gmail.com>
2022-02-19 16:11:34 +0700

    edit migration product

--- db/migrations.go
+++ db/migrations.go
@@ -3,9 +3,12 @@ package db
 import (
 	model "HarapanBangsaMarket/modules/user/model"
 	modelPromotion "HarapanBangsaMarket/modules/promotion/model"
+	modelProduct "HarapanBangsaMarket/modules/product/model"
 )
 
 func migrate() {
 	_ = Orm.AutoMigrate(new(model.User))
 	_ = Orm.AutoMigrate(new(modelPromotion.Promotion))
+	_ = Orm.AutoMigrate(new(modelProduct.ProductCategory))
+	_ = Orm.AutoMigrate(new(modelProduct.Product))
 }
