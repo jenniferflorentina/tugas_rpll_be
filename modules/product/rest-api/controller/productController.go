eb3bd2be1defec1ebb871ac9897c83c6911e40eb
Jennifer <jenniferflorentina@gmail.com>
2022-03-29 21:53:43 +0700

    feat: add controller for update image product

--- modules/product/rest-api/controller/productController.go
+++ modules/product/rest-api/controller/productController.go
@@ -5,6 +5,8 @@ import (
 	"HarapanBangsaMarket/mapper"
 	"HarapanBangsaMarket/modules/product/domain/model"
 	"HarapanBangsaMarket/modules/product/rest-api/dto"
+	"fmt"
+	"io/ioutil"
 	"net/http"
 	"strconv"
 
@@ -189,6 +191,61 @@ func DeleteProduct(c *fiber.Ctx) error {
 	}
 
 	products, err := service.DeleteProduct(id)
+	if err != nil {
+		e.HandleErr(c, err)
+		return nil
+	}
+
+	var DTO dto.ProductDTO
+	mapper.Map(products, &DTO)
+
+	_ = c.JSON(response.HTTPResponse{
+		Code: http.StatusOK,
+		Data: DTO,
+	})
+	return nil
+}
+
+func UpdateProductImage(c *fiber.Ctx) error {
+	_, authErr := auth.ExtractTokenMetadata(c)
+	if authErr != nil {
+		e.HandleErr(c, authErr)
+		return nil
+	}
+
+	id, err := strconv.ParseInt(c.Params("id"), 10, 64)
+	if err != nil {
+		e.HandleErr(c, err)
+		return nil
+	}
+
+	file, err := c.FormFile("image")
+
+	if err != nil {
+		return c.JSON(fiber.Map{"status": 500, "message": "Server error", "data": nil})
+	}
+
+	img, _:= file.Open()
+	fmt.Printf("Uploaded File: %+v\n", file.Filename)
+	fmt.Printf("File Size: %+v\n", file.Size)
+	fmt.Printf("MIME Header: %+v\n", file.Header)
+
+	// Create a temporary file within our temp-images directory that follows
+	// a particular naming pattern
+	tempFile, err := ioutil.TempFile("temp-images", "upload-*.png")
+	if err != nil {
+		fmt.Println(err)
+	}
+	defer tempFile.Close()
+
+	// read all of the contents of our uploaded file into a
+	// byte array
+	fileBytes, err := ioutil.ReadAll(img)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	products, err := service.UpdateProductImage(fileBytes, id)
 	if err != nil {
 		e.HandleErr(c, err)
 		return nil
